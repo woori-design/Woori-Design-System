015272e4b1842101af7415364aefa3cebc1cf135
mjgwon24 <alswlchlrh8@naver.com>
2025-04-09 18:56:16 +0900

    update: fontSize 8~10 추가

--- src/styles/foundation/typography/typography.type.ts
+++ src/styles/foundation/typography/typography.type.ts
@@ -2,7 +2,7 @@ import { CSSProperties } from "react";
 
 export type FontWeight = "Rg" | "Md" | "Sb";
 
-export type FontSize = 11 | 12 | 14 | 16 | 18 | 20 | 22 | 24 | 26 | 28 | 32 | 36 | 48;
+export type FontSize = 8 | 9 | 10 | 11 | 12 | 14 | 16 | 18 | 20 | 22 | 24 | 26 | 28 | 32 | 36 | 48;
 
 export type TypographyToken = `${FontWeight}_${FontSize}`;
 
