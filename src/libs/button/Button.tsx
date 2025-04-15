d42a43c86ddefa3af9c4e1f9a7d7409e930aa300
seocylucky <seocylucky@github.com>
2025-04-15 17:33:43 +0900

    feat: Button 컴포넌트 리뉴얼 작업

--- src/libs/button/Button.tsx
+++ src/libs/button/Button.tsx
@@ -4,14 +4,21 @@ import styles from "./Button.module.css";
 import { ButtonProps } from "./Button.type";
 
 const Button = forwardRef<HTMLButtonElement, ButtonProps>((props, ref) => {
-  const { size, width, children, onClick, ...rest } = props;
+  const { size, width, variant = "box", rounded = 16, children, onClick, ...rest } = props;
 
-  const sizeClassName = styles[`button--${size}`];
+  const className = [
+    styles.button,
+    styles[`button--${size}`],
+    variant === "text" && styles["button--text"],
+    styles[`button--rounded-${rounded}`],
+  ]
+    .filter(Boolean)
+    .join(" ");
 
   return (
     <button
       ref={ref}
-      className={`${styles.button} ${sizeClassName}`}
+      className={`${styles.button} ${className}`}
       disabled={props.disabled}
       style={width ? { width } : undefined}
       onClick={onClick}
