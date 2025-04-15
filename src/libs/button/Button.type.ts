9df20b2f1c85beb031e76f912f3b78e05e29b227
seocylucky <seocylucky@github.com>
2025-04-15 17:33:14 +0900

    feat: Button 타입 추가 (variant, rounded)

--- src/libs/button/Button.type.ts
+++ src/libs/button/Button.type.ts
@@ -1,7 +1,11 @@
 export type ButtonSize = "xlarge" | "large" | "medium" | "small" | "xsmall";
+export type ButtonVarient = "box" | "text";
+export type ButtonRounded = 16 | 100;
 
 export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   size: ButtonSize;
   width?: string;
   children?: React.ReactNode;
+  variant: ButtonVarient;
+  rounded?: ButtonRounded;
 }
