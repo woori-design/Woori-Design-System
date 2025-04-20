f8ca7558c3139263900d884b627fb99ff951f3c0
seocylucky <seocylucky@github.com>
2025-04-20 10:17:27 +0900

    fix: Switch story 및 문서화 수정

--- src/libs/switch/Switch.stories.tsx
+++ src/libs/switch/Switch.stories.tsx
@@ -1,155 +1,178 @@
 import { Meta, StoryObj } from "@storybook/react";
 import Switch from "./Switch";
+import { useArgs } from "@storybook/preview-api";
+import { useState } from "react";
 
 const meta: Meta<typeof Switch> = {
-    title: "libs/Switch",
-    component: Switch,
-    parameters: {
-        layout: "centered",
+  title: "libs/Switch",
+  component: Switch,
+  parameters: {
+    layout: "centered",
+  },
+  argTypes: {
+    checked: {
+      description: "Switch의 체크 상태",
+      control: {
+        type: "boolean",
+      },
     },
-    argTypes: {
-        checked: {
-            description: "Switch의 체크 상태",
-            control: {
-                type: "boolean",
-            },
-        },
-        onChange: {
-            description: "Switch의 상태 변경 이벤트",
-        },
-        pointText: {
-            description: "Switch의 포인트 텍스트",
-            control: {
-                type: "object",
-                options: [
-                    {
-                        on: "ON",
-                        off: "OFF",
-                    },
-                ],
-            }
-        },
-        pointColor: {
-            description: "Switch의 포인트 색상 변경 여부",
-            control: {
-                type: "boolean"
-            }
-        },
-        size: {
-            description: "Switch의 크기를 결정하는 속성",
-            control: {
-                type: "select",
-                options: ["small", "medium", "large", "xlarge"],
-            }
-        },
-        disabled: {
-            description: "Switch의 비활성화 상태",
-            control: {
-                type: "boolean",
-            },
-        },
-        className: {
-            description: "커스텀 클래스 이름",
-            control: {
-                type: "text",
-            },
-        },
-        style: {
-            description: "커스텀 스타일",
-            control: {
-                type: "object",
-            },
-        },
-    }
+    onCheckedChange: {
+      description: "Switch의 상태 변경 이벤트",
+    },
+    pointText: {
+      description: "Switch의 포인트 텍스트",
+      control: {
+        type: "object",
+      },
+    },
+    pointColor: {
+      description: "Switch의 포인트 색상 변경 여부",
+      control: {
+        type: "select",
+      },
+    },
+    size: {
+      description: "Switch의 크기를 결정하는 속성",
+      control: {
+        type: "select",
+        options: ["small", "medium", "large", "xlarge"],
+      },
+    },
+    disabled: {
+      description: "Switch의 비활성화 상태",
+      control: {
+        type: "boolean",
+      },
+    },
+  },
 };
 
 export default meta;
 type Story = StoryObj<typeof Switch>;
 
+export const Primary: Story = {
+  render: (args) => {
+    const [_, setArgs] = useArgs();
+
+    return (
+      <Switch
+        {...args}
+        checked={args.checked}
+        onCheckedChange={(val) => {
+          setArgs({ checked: val });
+          args.onCheckedChange?.(val);
+        }}
+      />
+    );
+  },
+  args: {
+    checked: true,
+    pointColor: "white",
+    pointText: {
+      on: "ON",
+      off: "OFF",
+    },
+  },
+};
+
+export const Size: Story = {
+  render: () => {
+    const [checkedStates, setCheckedStates] = useState({
+      xlarge: true,
+      large: true,
+      medium: true,
+      small: true,
+    });
+
+    const handleChange = (size: keyof typeof checkedStates) => (val: boolean) => {
+      setCheckedStates((prev) => ({ ...prev, [size]: val }));
+    };
+
+    return (
+      <div style={{ display: "flex", gap: "10px", alignItems: "end" }}>
+        <Switch
+          size="xlarge"
+          checked={checkedStates.xlarge}
+          onCheckedChange={handleChange("xlarge")}
+        />
+        <Switch
+          size="large"
+          checked={checkedStates.large}
+          onCheckedChange={handleChange("large")}
+        />
+        <Switch
+          size="medium"
+          checked={checkedStates.medium}
+          onCheckedChange={handleChange("medium")}
+        />
+        <Switch
+          size="small"
+          checked={checkedStates.small}
+          onCheckedChange={handleChange("small")}
+        />
+      </div>
+    );
+  },
+};
+
 export const Checked: Story = {
-    args: {
-        checked: true,
-        disabled: false,
-        pointColor: false,
-        onChange: (newVal) => {
-            console.log(newVal);
-        },
-    }
-}
+  render: () => {
+    return (
+      <Switch
+        size="medium"
+        onCheckedChange={(val) => {
+          alert(`Switch 체크 여부: ${val}`);
+        }}
+      />
+    );
+  },
+};
 
-export const Unchecked: Story = {
-    args: {
-        checked: false,
-        disabled: false,
-        pointColor: false,
-        onChange: (newVal) => {
-            console.log(newVal);
-        },
-    }
-}
+export const Disabled: Story = {
+  render: () => (
+    <div style={{ display: "flex", gap: "10px", alignItems: "end" }}>
+      <div style={{ display: "flex", flexDirection: "column", gap: "5px", alignItems: "center" }}>
+        <h4>checked/White Ver.</h4>
+        <Switch size="medium" checked={true} disabled={true} />
+      </div>
+      <div style={{ display: "flex", flexDirection: "column", gap: "5px", alignItems: "center" }}>
+        <h4>unChecked/White Ver.</h4>
+        <Switch size="medium" checked={false} disabled={true} />
+      </div>
+      <div style={{ display: "flex", flexDirection: "column", gap: "5px", alignItems: "center" }}>
+        <h4>checked/Color Ver.</h4>
+        <Switch size="medium" checked={true} disabled={true} pointColor="color" />
+      </div>
+      <div style={{ display: "flex", flexDirection: "column", gap: "5px", alignItems: "center" }}>
+        <h4>unChecked/Color Ver.</h4>
+        <Switch size="medium" checked={false} disabled={true} pointColor="color" />
+      </div>
+    </div>
+  ),
+};
 
-export const CheckedWithPointColor: Story = {
-    args: {
-        checked: true,
-        disabled: false,
-        pointColor: true,
-        onChange: (newVal) => {
-            console.log(newVal);
-        },
-    }
-}
+export const PointColor: Story = {
+  render: () => {
+    return (
+      <div style={{ display: "flex", gap: "10px", alignItems: "end" }}>
+        <div style={{ display: "flex", flexDirection: "column", gap: "5px", alignItems: "center" }}>
+          <h4>White Ver.</h4>
+          <Switch size="medium" />
+        </div>
+        <div style={{ display: "flex", flexDirection: "column", gap: "5px", alignItems: "center" }}>
+          <h4>Color Ver.</h4>
+          <Switch size="medium" pointColor="color" />
+        </div>
+      </div>
+    );
+  },
+};
 
-export const UncheckedWithPointColor: Story = {
-    args: {
-        checked: false,
-        disabled: false,
-        pointColor: true,
-        onChange: (newVal) => {
-            console.log(newVal);
-        },
-    }
-}
-
-export const CheckedDisabled: Story = {
-    args: {
-        checked: true,
-        disabled: true,
-        pointColor: false,
-        onChange: (newVal) => {
-            console.log(newVal);
-        },
+export const PointText: Story = {
+  args: {
+    pointText: {
+      on: "켬",
+      off: "끔",
     },
-}
-
-export const UncheckedDisabled: Story = {
-    args: {
-        checked: false,
-        disabled: true,
-        pointColor: false,
-        onChange: (newVal) => {
-            console.log(newVal);
-        },
-    },
-}
-
-export const CheckedDisabledWithPointColor: Story = {
-    args: {
-        checked: true,
-        disabled: true,
-        pointColor: true,
-        onChange: (newVal) => {
-            console.log(newVal);
-        },
-    },
-}
-
-export const UncheckedDisabledWithPointColor: Story = {
-    args: {
-        checked: false,
-        disabled: true,
-        pointColor: true,
-        onChange: (newVal) => {
-            console.log(newVal);
-        },
-    },
-}
+  },
+};
