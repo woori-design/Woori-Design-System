9b72b04cee39a2a092d932e593c66a6fa1b7e51e
조윤주 <155442976+iamyuunzo@users.noreply.github.com>
2025-04-25 23:55:45 +0900

    refactor: Divider 컴포넌트 리팩토링 및 Storybook 문서 작성 (#13)
    * refactor: Divider 컴포넌트 리팩토링 및 문서 파일 정리
    
    * fix: case sensitivity divider.tsx → Divider.tsx
    
    * fix: Divider.tsx 최신 코드 반영
    
    * fix: 스토리북 문서화 수정
    
    * remove: types 파일 삭제
    
    * fix: style props 추가 및 border: none 제거
    
    * remove: divider css 파일  삭제
    
    * fix: css 파일 관련 코드 삭제
    
    * fix: control 자동 반영되도록 스토리북 수정
    
    ---------
    
    Co-authored-by: seocylucky <seocylucky@github.com>

---
+++ src/libs/divider/Divider.stories.tsx
@@ -0,0 +1,83 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import Divider from "./Divider";
+
+const meta = {
+  title: "libs/Divider",
+  component: Divider,
+  parameters: {
+    layout: "centered",
+    docs: {
+      description: {
+        component:
+          "화면을 구분하는 선을 표시하는 컴포넌트입니다. 디자인 시스템의 색상에 따라 스타일링됩니다.",
+      },
+    },
+  },
+  tags: ["autodocs"],
+  argTypes: {
+    width: {
+      description: "Divider의 길이",
+      control: { type: "text" },
+    },
+    thickness: {
+      description: "Divider의 두께",
+      control: { type: "number", min: 1, max: 99 },
+    },
+    style: {
+      table: {
+        disable: true,
+      },
+    },
+  },
+} satisfies Meta<typeof Divider>;
+
+export default meta;
+
+type Story = StoryObj<typeof Divider>;
+
+export const Primary: Story = {
+  args: {
+    width: "300px",
+    thickness: 2,
+  },
+};
+
+export const Default: Story = {
+  render: () => (
+    <div style={{ width: "800px" }}>
+      <Divider width="100%" thickness={1} />
+    </div>
+  ),
+};
+
+export const Width: Story = {
+  render: () => (
+    <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+      <h4>width: 800px</h4>
+      <Divider width="800px" />
+    </div>
+  ),
+};
+
+export const Thickness: Story = {
+  render: () => (
+    <div style={{ display: "flex", flexDirection: "column", gap: "24px" }}>
+      <div style={{ display: "flex", flexDirection: "column", gap: "2px", alignItems: "center" }}>
+        <h4>thinckness: 1px</h4>
+        <Divider width="300px" thickness={1} />
+      </div>
+      <div style={{ display: "flex", flexDirection: "column", gap: "2px", alignItems: "center" }}>
+        <h4>thinckness: 2px</h4>
+        <Divider width="300px" thickness={2} />
+      </div>
+      <div style={{ display: "flex", flexDirection: "column", gap: "2px", alignItems: "center" }}>
+        <h4>thinckness: 3px</h4>
+        <Divider width="300px" thickness={3} />
+      </div>
+      <div style={{ display: "flex", flexDirection: "column", gap: "2px", alignItems: "center" }}>
+        <h4>thinckness: 4px</h4>
+        <Divider width="300px" thickness={4} />
+      </div>
+    </div>
+  ),
+};
