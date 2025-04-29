622181a4cfc0f993f23ebcada46fcf59b605ce0f
seocylucky <seocylucky@github.com>
2025-04-29 15:44:14 +0900

    fix: Checkbox 속성 관련 수정 및 helperText 관련 코드 제거

--- src/libs/checkbox/Checkbox.tsx
+++ src/libs/checkbox/Checkbox.tsx
@@ -1,53 +1,46 @@
-import React, { forwardRef } from "react";
+import { forwardRef } from "react";
 import styles from "./Checkbox.module.css";
 import { CheckboxProps } from "./Checkbox.type";
 
-const Checkbox = forwardRef<HTMLInputElement, CheckboxProps>(
-  (
-    {
-      label,
-      checked = false,
-      onChange,
-      type = "default",
-      shape = "square",
-      disabled = false,
-      helperText,
-      ...props
-    },
-    ref
-  ) => {
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-      onChange?.(e.target.checked);
-    };
+const Checkbox = forwardRef<HTMLInputElement, CheckboxProps>((props, ref) => {
+  const {
+    size,
+    label,
+    checked,
+    onChange,
+    type = "default",
+    shape = "square",
+    disabled = false,
+    ...rest
+  } = props;
 
-    const className = [
-      styles.checkboxContainer,
-      styles.checkbox,
-      styles[type],
-      styles[shape],
-      disabled && styles.disabled,
-    ]
-      .filter(Boolean)
-      .join(" ");
+  const className = [
+    styles.checkboxContainer,
+    styles.checkbox,
+    styles[type],
+    styles[shape],
+    styles[size],
+    disabled && styles.disabled,
+  ]
+    .filter(Boolean)
+    .join(" ");
 
-    return (
+  return (
+    <>
       <label className={`${className}`}>
         <input
           ref={ref}
           type="checkbox"
           checked={checked}
-          onChange={handleChange}
+          onChange={onChange}
           disabled={disabled}
           className={styles.input}
-          {...props}
+          {...rest}
         />
-        <div className={styles.textWrapper}>
-          <span className={styles.label}>{label}</span>
-          {helperText && <div className={styles.helperText}>{helperText}</div>}
-        </div>
+        <span className={styles.label}>{label}</span>
       </label>
-    );
-  }
-);
+    </>
+  );
+});
 
 export default Checkbox;
