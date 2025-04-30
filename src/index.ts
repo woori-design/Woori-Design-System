7bcc6cc29d095abe8b23d97fe5fcebce3d60808a
seocylucky <seocylucky@github.com>
2025-04-30 23:02:32 +0900

    feat: index.ts와 index.css에 컴포넌트 및 스타일 파일 추가

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,5 @@ export { theme } from "./styles/theme/theme";
 export { default as FloatButton } from "./libs/floatbutton/FloatButton";
 export { default as MenuItem } from "./libs/floatbutton/MenuItem";
 export { default as Divider } from "./libs/divider/Divider";
-export { default as Checkbox } from "./libs/checkbox/Checkbox";
 export { default as Button } from "./libs/button/Button";
+export { default as Switch } from "./libs/switch/Switch";
