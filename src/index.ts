1a28743b13f152a4afcfec26646609890170b4de
Chaeyeon Seo <94633589+seocylucky@users.noreply.github.com>
2025-07-02 20:26:43 +0900

    feat: Global CSS Typo, Color 변수화 (#30)
    * feat: globalSemantiColor css 파일 생성
    
    * feat: 컴포넌트 css color 전역 변수로 일괄 변경
    
    * feat: storybook과 index.ts에서 전역 변수 불러오도록 작업

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import "./index.css";
+import "./styles/foundation/color/globalSemanticColors.css";
 
 export { theme } from "./styles/theme/theme";
 export { default as FloatButton } from "./libs/floatbutton/FloatButton";
