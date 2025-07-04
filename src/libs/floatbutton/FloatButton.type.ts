70752d056df4e59a5154b0ffe02b264d625795a4
seocylucky <seocylucky@github.com>
2025-07-04 21:54:10 +0900

    feat: position, offset 타입 추가

--- src/libs/floatbutton/FloatButton.type.ts
+++ src/libs/floatbutton/FloatButton.type.ts
@@ -1,14 +1,27 @@
 import { ButtonHTMLAttributes, ReactElement } from "react";
 import { MenuItemProps } from "./MenuItem.type";
 
+export type FloatButtonSize = "sm" | "md" | "lg" | "xl";
+export type FloatButtonShape = "circle" | "square";
+export type FloatPosition = "right-top" | "left-top" | "right-bottom" | "left-bottom";
+
+export interface Offset {
+  x: number;
+  y: number;
+}
+
 /**
  * 플로팅 버튼 속성 인터페이스
  */
 interface FloatButtonOwnProps {
   /** 버튼 크기 */
-  size?: "sm" | "md" | "lg" | "xl";
+  size?: FloatButtonSize;
   /** 버튼 모양 */
-  shape?: "circle" | "square";
+  shape?: FloatButtonShape;
+  /** 위치 지정 (포탈 기준) */
+  position?: FloatPosition;
+  /** 위치 오프셋 (x: 좌우 거리, y: 상하 거리) */
+  offset?: Offset;
   /** 자식 요소 (MenuItem 컴포넌트만 허용) */
   children: ReactElement<MenuItemProps>[];
   /** 메뉴 상태 변경 콜백 - 열림/닫힘 상태 전달 */
@@ -16,4 +29,4 @@ interface FloatButtonOwnProps {
 }
 
 export type FloatButtonProps = FloatButtonOwnProps &
-    Omit<ButtonHTMLAttributes<HTMLButtonElement>, "onToggle">;
+  Omit<ButtonHTMLAttributes<HTMLButtonElement>, "onToggle">;
