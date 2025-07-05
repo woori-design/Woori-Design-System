f292fff8a061d729f7c594e3a04854d10e0da252
seocylucky <seocylucky@github.com>
2025-07-05 22:07:16 +0900

    fix: react/jsx-runtime external 설정 추가

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
       fileName: (format) => `index.${format}.js`,
     },
     rollupOptions: {
-      external: ["react", "react-dom"],
+      external: ["react", "react-dom", "react/jsx-runtime"],
       output: {
         assetFileNames: "index.[ext]",
         globals: {
