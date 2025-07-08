7f3b5cb02a9debec724e73f34c9e6d790ba81e5b
조윤주 <155442976+iamyuunzo@users.noreply.github.com>
2025-07-08 20:49:13 +0900

    feat: Dropdown 컴포넌트 구현

--- src/index.ts
+++ src/index.ts
@@ -7,3 +7,4 @@ export { default as Divider } from "./libs/divider/Divider";
 export { default as Checkbox } from "./libs/checkbox/Checkbox";
 export { default as Button } from "./libs/button/Button";
 export { default as Switch } from "./libs/switch/Switch";
+export { default as Dropdown } from "./libs/dropdown/Dropdown";
---
+++ src/libs/dropdown/Dropdown.type.ts
@@ -0,0 +1,5 @@
+export interface DropdownProps {
+  placeholder?: string;
+  options: string[];
+  width?: string;
+}
